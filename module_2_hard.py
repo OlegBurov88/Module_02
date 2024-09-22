6860e4606c410a4428fb3e88cd4329bdffa7ca7c
OlegBurov88 <ovburov88@gmail.com>
2024-09-22 19:23:14 +0500

    Дополнительное практическое задание по модулю*

---
+++ module_2_hard.py
@@ -0,0 +1,15 @@
+def password(n):
+    code = ''
+    for i in range(1, n):
+        for j in range(i, n):
+            if n % (i + j) == 0 and i != j:
+                code += str(i) + str(j)
+    return code
+
+
+n = int(input('Введите число от 3 до 20: '))
+if 2 < n < 21:
+    code = password(n)
+    print(code)
+else:
+    print('WASTED')
