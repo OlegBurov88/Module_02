3e06f61f5978576ac20dfcc85ec572e13e608596
OlegBurov88 <ovburov88@gmail.com>
2024-09-29 10:11:52 +0500

    Вебинар по модулю "Основные операторы"

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ x = 43
 y = 32
 print(x * y)
 print("End line")
+
+
+
+mouth = 4
+
+match mouth:
+    case 1:
+        print('Январь')
+    case 2:
+        print('Февраль')
+    case 3:
+        print('Март')
+    case 4:
+        print('Апрель')
